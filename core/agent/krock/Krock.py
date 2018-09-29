47b6f5516d968ed38e00c9ac6ee45c870493c868
FrancescoSaverioZuppichini <francesco.zuppichini@gmail.com>
2018-09-29 13:30:18 +0200

    added more callbacks in krock agent

--- core/agent/krock/Krock.py
+++ core/agent/krock/Krock.py
@@ -1,51 +1,56 @@
 import rospy
+import numpy as np
 
 from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Wrench, PoseStamped, TwistStamped
 from nav_msgs.msg import Odometry
-from webots_ros.msg import Int8Stamped
+from webots_ros.msg import Int8Stamped, Float64ArrayStamped
 from sensor_msgs.msg import JointState, Joy
 from std_msgs.msg import String, Header
-import numpy as np
+from tf import transformations
+
 from agent import RospyAgent
 
 class Krock(RospyAgent):
     BASE_TOPIC = '/krock'
     POSE_SUB = '{}/pose'.format(BASE_TOPIC)
     GAIT_CHOOSER = '{}/gait_chooser'.format(BASE_TOPIC)
+    MANUAL_CONTROL = '{}/manual_control_input'.format(BASE_TOPIC)
+    STATUS = '{}/status'.format(BASE_TOPIC)
+    SPAWN = '{}/spawn_pose'.format(BASE_TOPIC)
+    TOUCH_SENSOR = '{}/touch_sensors'.format(BASE_TOPIC)
+    TORQUES_FEEDBACK = '{}/torques_feedback'.format(BASE_TOPIC)
 
     def init_publishers(self):
         return {
             'joy' : rospy.Publisher('/joy', Joy, queue_size=1),
-            'gait': rospy.Publisher(self.GAIT_CHOOSER, Int8Stamped, queue_size=1)
-
+            'gait': rospy.Publisher(self.GAIT_CHOOSER, Int8Stamped, queue_size=1),
+            'manual_control': rospy.Publisher(self.MANUAL_CONTROL, Float64ArrayStamped, queue_size=1),
+            'status': rospy.Publisher(self.STATUS, String, queue_size=1),
+            'spawn': rospy.Publisher(self.SPAWN, PoseStamped, queue_size=1)
         }
 
     def init_subscribers(self):
         rospy.Subscriber(self.POSE_SUB, PoseStamped, self.callback_pose)
-        rospy.Subscriber('/joy', Joy, self.callback_joy)
-
-    def callback_joy(self, data):
-        self.state['joy'] = data
-
+        rospy.Subscriber(self.TOUCH_SENSOR, Float64ArrayStamped, self.callback_touch_sensors)
+        rospy.Subscriber(self.TORQUES_FEEDBACK, Float64ArrayStamped, self.callback_torques_feedback)
 
     def callback_pose(self, data):
         self.state['pose'] = data
-        # rospy.loginfo("Pos /data received")
-        # self.state['pose'] = data
 
-        return 0
+    def callback_touch_sensors(self, data):
+        pass
 
-    def move(self, gait=1):
-        # self.publishers['gait'].publish(Int8Stamped(data=gait))
-        buttons = np.zeros(11, dtype=np.int)
-        # buttons[1] = 1
-        # buttons[2] = 0
+    def callback_torques_feedback(self, data):
+        pass
 
-        msg = Joy(axes=[0,-0.5,0.5,0],
-                  buttons=list(buttons))
+    def move(self, gait, frontal_freq, lateral_freq, manual_mode=False):
+        mode = int(manual_mode)
+        msg = Float64ArrayStamped(data=[mode, gait, frontal_freq, lateral_freq])
+        self.publishers['manual_control'].publish(msg)
 
-        self.publishers['joy'].publish(msg)
-        # print('pub')
+    def spawn(self, pos=None):
+        pos = pos if pos != None else PoseStamped(pose = generate_random_pose())
+        self.publishers['spawn'].publish(pos)
 
     def on_shut_down(self):
         self.notify('on_shut_down')
--- core/test.py
+++ core/test.py
@@ -1,18 +1,42 @@
 import rospy
+import numpy as np
+
+from tf import transformations
+
 from agent.krock import Krock
 from agent.callbacks import RosBagSaver
 
 rospy.init_node("record_single_trajectory")
 
-r = rospy.Rate(hz=10)
+map_max_x = 5.0
+map_max_y = 5.0
+map_max_z = 1.0
+# TODO this stuff should go in utils or in simlation class
+def generate_random_pose():
+    # x,y (z will be fixed as the max_hm_z so that the robot will drop down), gamma as orientation
+    rn = np.random.random_sample((3,))
+    random_pose = Pose()
+    random_pose.position.x = 2 * map_max_x *rn[0] - map_max_x
+    random_pose.position.y = 2 * map_max_x *rn[1] - map_max_x
+    random_pose.position.z = map_max_z * 1.0 # spawn on the air
+    qto = transformations.quaternion_from_euler(0, 0, 2*np.pi * rn[1], axes='sxyz')
+    random_pose.orientation.x = qto[0]
+    random_pose.orientation.y = qto[1]
+    random_pose.orientation.z = qto[2]
+    random_pose.orientation.w = qto[3]
+    return random_pose
 
+r = rospy.Rate(hz=0.5)
 k = Krock()
 
 k.add_callback(RosBagSaver('./data.bag'))
 k()
+# r.sleep()
+# k.spawn()
 
 while not rospy.is_shutdown():
-    k.move(2)
+    k.spawn()
+#     # k.move()
     r.sleep()
 
 rospy.on_shutdown(k.on_shut_down)
