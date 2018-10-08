4be3f12865ed47d6634a0d75fd72e65e6cc7934b
Fra <zuppif@usi.ch>
2018-10-08 22:33:30 +0200

    Redefined the interaction between Agent and Simulation. Agent now has a 'act' method that decides how to move itself in the world

--- core/agent/Agent.py
+++ core/agent/Agent.py
@@ -2,20 +2,61 @@ from protocols import Callbackable
 from .callbacks import AgentCallback
 
 class Agent(Callbackable, AgentCallback):
+    """
+    Basic Agent inferface. An agent is an entity that interacts with a world,
+    and environment.
+    It is defined by the action it can do. This interfaces exposes some basic
+    interaction such as 'move'.
+    # TODO I should add a Brain that decide how to act based on the enviroment
+    """
     def __init__(self):
         self.state = {}
         self.set_callbacks([self])
 
+    # TODO what about pass the world param?
     def spawn(self, pos):
+        """
+        Spawn the robot in the world.
+        :param pos:
+        :return:
+        """
         pass
 
+    # TODO what about pass the world param?
     def move(self, *args, **kwargs):
+        """
+        This function moves the agent in the world. It depends on the
+        internet implementation and the simulator used. However, it should be
+        as genere as possible.
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        pass
+
+    def act(self, world, *args, **kwargs):
+        """
+        This function should do something based on the world the agent
+        is in. Ideally it should move it based on some input.
+        :param world:
+        :param args:
+        :param kwargs:
+        :return:
+        """
         pass
 
     def stop(self):
+        """
+        Stop the agent.
+        :return:
+        """
         pass
 
     def sleep(self):
+        """
+        This function can be useful to allow the robot to idle.
+        :return:
+        """
         pass
 
     def die(self):
--- core/main.py
+++ core/main.py
@@ -1,18 +1,13 @@
 import rospy
 import time
 import tqdm
-import subprocess
 
 from agent.krock import Krock
 from agent.callbacks import RosBagSaver
 
-from simulation import Simulation
 from simulation.callbacks import *
 
-from webots import WebotsSimulation
-
 from world import *
-from utils import Supervisor
 
 from webots import *
 
@@ -27,12 +22,14 @@ rospy.init_node("traversability_simulation")
 w = World(file_path=WORLD)
 w()
 
+
 def create_agent():
     krock = Krock()
     krock.add_callback(RosBagSaver('./data/{}.bag'.format(time.time()),
                                    topics=['pose']))
     krock()
     return krock
+
 
 sim = WebotsSimulation(name=args.robot)
 sim.add_callbacks([Alarm(stop_after_s=SIM_TIME),
@@ -41,7 +38,8 @@ sim.add_callbacks([Alarm(stop_after_s=SIM_TIME),
 bar = tqdm.tqdm(range(N_SIM))
 bar.set_description('Running simulations')
 
-for _ in bar:
-    a = create_agent()
-    sim(world=w,
-        agent=a)
+with bar as b:
+    for _ in b:
+        a = create_agent()
+        sim(world=w,
+            agent=a)
