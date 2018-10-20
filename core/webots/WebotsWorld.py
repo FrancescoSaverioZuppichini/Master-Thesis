d6a816f79d6f0ce2d554c1e26e272c9e75ed231b
FrancescoSaverioZuppichini <zuppif@usi.ch>
2018-10-20 09:43:41 +0200

    refactor World and WebotsWorld to expose a random_position

--- core/webots/WebotsWorld.py
+++ core/webots/WebotsWorld.py
@@ -1,6 +1,9 @@
+import numpy as np
+
 from world import World
 from utils.webots2ros import *
-import time
+from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Wrench, PoseStamped, TwistStamped
+from tf import transformations
 
 class WebotsWorld(World, Supervisor):
     name = '/krock'
@@ -21,3 +24,28 @@ class WebotsWorld(World, Supervisor):
 
         self.x = self.x_dim * self.x_spac
         self.y = self.y_dim * self.y_spac
+
+    @property
+    def random_position(self):
+        map_x = (0, self.x)
+        map_y = (0, self.y)
+
+        rx = np.random.uniform(*map_x)
+        ry = np.random.uniform(*map_y)
+
+        random_pose = Pose()
+        random_pose.position.x = rx
+        random_pose.position.y = ry
+        # to get the 2d index in 1d matrix x + width * y
+        idx = (rx // self.x_spac )+ (1600 * int(ry // self.y_spac))
+
+        h = self.grid['height'][idx].value
+
+        random_pose.position.z = h + 0.5
+        # print(random_pose.position.z )
+        qto = transformations.quaternion_from_euler(0, 0, 2 * np.pi * np.random.uniform(0, 1), axes='sxyz')
+        random_pose.orientation.x = qto[0]
+        random_pose.orientation.y = qto[1]
+        random_pose.orientation.z = qto[2]
+        random_pose.orientation.w = qto[3]
+        return random_pose
--- core/world/World.py
+++ core/world/World.py
@@ -12,10 +12,6 @@ class World():
     def __init__(self, file_path):
         self.file_path = file_path
 
-    @property
-    def path(self):
-        return Path(self.file_path)
-
     def __call__(self, *args, **kwargs):
         """
         This method should initialise the world. E.g. open file,
@@ -25,3 +21,16 @@ class World():
         :return:
         """
         pass
+
+    # TODO probably it is better to add a class method to CREATE the world from a file
+    @property
+    def path(self):
+        return Path(self.file_path)
+
+    @property
+    def random_position(self):
+        """
+        This function must return a random position inside the world
+        :return:
+        """
+        return None
