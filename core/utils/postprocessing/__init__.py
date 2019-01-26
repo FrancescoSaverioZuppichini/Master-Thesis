36989060c7c4680af539ff2425905ab5cf59064c
Fra <zuppif@usi.ch>
2019-01-26 15:47:26 +0100

    fixed!

--- core/utils/postprocessing/__init__.py
+++ core/utils/postprocessing/__init__.py
@@ -1,2 +1,2 @@
-from .utils import *
 from .config import *
+from .utils import *
--- core/utils/postprocessing/main.py
+++ core/utils/postprocessing/main.py
@@ -1,15 +1,17 @@
 import glob
 
+
 from config import Config
 from bags2csvs import bags2csvs
 from csvs2dataset import csvs2dataset
-#
-# bags = glob.glob(Config.BAG_FOLDER + '/flat/**.bag')
-# bags2csvs(bags)
+
+
+bags = glob.glob(Config.BAG_FOLDER + '/*/**.bag')
+bags2csvs(bags)
 
 
 # csvs = glob.glob(Config.CSV_FOLDER + '/flat/1542573200.115245.csv')
-# csvs = glob.glob(Config.CSV_FOLDER + '/flat/**.csv')
-csvs = glob.glob(Config.CSV_FOLDER + '/bumps1/1542445123.896269.csv')
+csvs = glob.glob(Config.CSV_FOLDER + '/*/**.csv')
+# csvs = glob.glob(Config.CSV_FOLDER + '/flat-block/*.csv')
 
 csvs2dataset(csvs)
