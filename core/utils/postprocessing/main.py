5f0bd416e7b5cb31d4b8f9524346b623d18ae747
Fra <zuppif@usi.ch>
2019-01-28 10:53:20 +0100

    improved postprocessing pipeline first model draf dataset impelemntation

--- core/utils/postprocessing/main.py
+++ core/utils/postprocessing/main.py
@@ -1,12 +1,12 @@
 import glob
 
 from bags2csvs import *
-from csvs2dataset import *
+from dfs2dataset import *
 from config import Config
 
 bags = glob.glob(Config.BAG_FOLDER + '/flat/*.bag')
 stage = bags2dfs(bags)
-stage = csvs2dataset(stage)
-stage = csvs2paths(stage)
+stage = dfs2traversability_df(stage)
+stage = traversability_dfs2paths(stage)
 
 list(stage)
