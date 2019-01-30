b2ec8635d71e3ebcca4abb31550ce720f2abe04d
Fra <zuppif@usi.ch>
2019-01-30 19:14:54 +0100

    added query world file

--- core/forward.py
+++ core/forward.py
@@ -3,7 +3,7 @@ import pprint
 
 from env.webots.krock import Krock, KrockWebotsEnv
 
-WORLD_PATH = '/home/francesco/Documents/Master-Thesis/core/env/webots/krock/krock2_ros/worlds/flat.wbt'
+WORLD_PATH = '/home/francesco/Documents/Master-Thesis/core/env/webots/krock/krock2_ros/worlds/holes1.wbt'
 MAP = '/home/francesco/Desktop/querry-big.png'
 N_STEPS = 4
 
@@ -16,6 +16,7 @@ env = KrockWebotsEnv.from_image(
      'resolution': 0.02},
     output_dir='/home/francesco/Documents/Master-Thesis/core/env/webots/krock/krock2_ros/worlds/')
 
+# env = KrockWebotsEnv.from_file(WORLD_PATH)
 # spawn the robot at a random location
 init_obs = env.reset()
 
@@ -28,7 +29,7 @@ while True:
     env.reset()
     for _ in range(50):
         obs, r, done, _ = env.step(env.GO_FORWARD)
-        # pprint.pprint(obs)
+        pprint.pprint(obs)
         if done: break
 
     env.step(env.STOP)
