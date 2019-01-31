f362e446a498b40d8f6bbd4cc3cf54997535b2a2
Fra <zuppif@usi.ch>
2019-01-31 14:44:32 +0100

    added test dataloader

--- core/estimators/train.py
+++ core/estimators/train.py
@@ -13,10 +13,16 @@ from models.resnet import *
 from models.omar_cnn import OmarCNN
 
 torch.backends.cudnn.benchmark = True
-
 torch.backends.cudnn.deterministic = True
-
 torch.manual_seed(0)
+
+params = {'epoches': 20,
+          'lr': 0.0001,
+          'batch_size': 128,
+          'model': 'resnet18-pretrained=False',
+          'dataset': 'medium-h-center',
+          'resize': '80'}
+
 if torch.cuda.is_available(): torch.cuda.manual_seed_all(0)
 
 # model = OmarCNN()
@@ -30,16 +36,14 @@ model = TraversabilityResnet(1, block=BasicBlock, blocks=[2, 2, 2, 2],
 
 criterion = CrossEntropyFlat()
 
-train_dl, test_dl = get_dataloaders('/home/francesco/Desktop/data/dataset/images-medium-h-center')
+train_dl, test_dl = get_dataloaders(train_root='/home/francesco/Desktop/data/dataset/train/images-medium',
+                                    test_root='/home/francesco/Desktop/data/dataset/test/images/',
+                                    batch_size=params['batch_size'],
+                                    num_workers=16,
+                                    pin_memory=True)
 
 data = DataBunch(train_dl=train_dl, valid_dl=test_dl)
 
-params = {'epoches': 20,
-          'lr': 0.0001,
-          'batch_size': 128,
-          'model': 'resnet18-pretrained=False',
-          'dataset': 'medium-h-center',
-          'resize': '80'}
 
 print("train size={}, val size ={}".format(len(train_dl) * params['batch_size'], len(test_dl) * params['batch_size']))
 
