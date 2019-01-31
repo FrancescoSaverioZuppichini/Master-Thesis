273cf55874fcb9850385091dcdf99d9143a40fe2
Fra <zuppif@usi.ch>
2019-01-31 14:44:32 +0100

    added test dataloader

--- core/estimators/datasets/TraversabilityDataset.py
+++ core/estimators/datasets/TraversabilityDataset.py
@@ -7,31 +7,34 @@ from torchvision.datasets import ImageFolder
 TRAIN_SIZE = 0.8
 TEST_SIZE = 0.2
 BATCH_SIZE = 128
-N_WORKERS = 16
 
 transform = Compose([Grayscale(), ToTensor()])
 
 
-def get_dataloaders(root):
+def get_dataloaders(train_root, test_root, val_size=0.2, *args, **kwargs):
     """
     Get train and test dataloader. Due to the specific task,
-    we cannot apply data-augmentation (vlip, hflip, gamma...)
-    :return: train and test dataloaders
+    we cannot apply data-augmentation (vlip, hflip, gamma...).
+    The test set is composed entirely by maps never seen by
+    the model in the train set.
+    :return: train, val and test dataloaders
     """
-    ds = ImageFolder(root=root,
+    ds = ImageFolder(root=train_root,
                      transform=transform)
 
-    train_size = int(len(ds) * TRAIN_SIZE)
+    train_size = int(len(ds) * (1 - val_size))
 
-    train_ds, test_ds = random_split(ds, [train_size, len(ds) - train_size])
+    train_ds, val_ds = random_split(ds, [train_size, len(ds) - train_size])
 
     train_dl = DataLoader(train_ds,
-                          batch_size=BATCH_SIZE,
-                          num_workers=N_WORKERS,
-                          shuffle=True)
+                          shuffle=True,
+                          *args, **kwargs)
 
-    test_dl = DataLoader(test_ds,
-                         batch_size=BATCH_SIZE,
-                         num_workers=N_WORKERS)
+    val_dl = DataLoader(val_ds, *args, **kwargs)
 
-    return train_dl, test_dl
+    test_ds = ImageFolder(root=train_root,
+                     transform=transform)
+
+    test_dl = DataLoader(test_ds, *args, **kwargs)
+
+    return train_dl, val_dl, test_dl
