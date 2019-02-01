4f48b6d49cd20b10a18e5d76b4115517d88c86f3
Fra <zuppif@usi.ch>
2019-02-01 13:50:45 +0100

    80% on querry map

--- core/estimators/train.py
+++ core/estimators/train.py
@@ -18,16 +18,16 @@ torch.backends.cudnn.benchmark = True
 torch.backends.cudnn.deterministic = True
 torch.manual_seed(0)
 
-params = {'epoches': 25,
+params = {'epoches': 100,
           'lr': 0.0001,
           'batch_size': 128,
-          'model': 'resnet18-pretrained=True',
-          'dataset': 'new-medium',
+          'model': 'resnet18-pretrained',
+          'dataset': '100-80-0.09',
           'resize': '80'}
 
 if torch.cuda.is_available(): torch.cuda.manual_seed_all(0)
 
-model = OmarCNN()
+# model = OmarCNN()
 # model = TraversabilityResnet(1, block=BasicBlock, blocks=[2, 2, 2, 2],
 #                              preactivated=False).cuda()
 
@@ -40,7 +40,8 @@ print(model)
 criterion = CrossEntropyFlat()
 
 train_dl, val_dl, test_dl = get_dataloaders(train_root='/home/francesco/Desktop/data/train/dataset/new-medium',
-                                    test_root='/home/francesco/Desktop/data/test/dataset/new-medium',
+                                    test_root='/home/francesco/Desktop/data/test/dataset/{}'.format(params['dataset']),
+                                    val_size=0.15,
                                     batch_size=params['batch_size'],
                                     num_workers=16,
                                     pin_memory=True)
@@ -65,17 +66,20 @@ learner = Learner(data=data,
                   model_dir='/home/francesco/Desktop/carino/vaevictis/data/',
                   loss_func=criterion,
                   metrics=[accuracy])
-
+#
 try:
     with experiment.train():
         # learner.fit(epochs=params['epoches'], lr=params['lr'])
-        learner.fit(epochs=4, lr=params['lr'])
+        learner.fit(epochs=2, lr=params['lr'])
         model.layers.requires_grad = True
-        learner.fit(epochs=4, lr=params['lr']/10)
-
-    learner.save(params['model'])
-
-except KeyboardInterrupt:
+        learner.fit(epochs=2, lr=params['lr']/10)
+    #
+except:
     pass
+
+learner.save(params['model'])
+
 with experiment.test():
-    print(learner.validate(data.test_dl, metrics=[accuracy]))
+    loss, acc = learner.validate(data.test_dl, metrics=[accuracy])
+    print(loss, acc)
+    experiment.log_metric("accuracy", acc.item())
