8a7d5eec4f3004963351c6a7b798b568cf76f1a9
FrancescoSaverioZuppichini <zuppif@usi.ch>
2019-02-02 15:50:38 +0100

    minor changes

--- core/utils/postprocessing/config.py
+++ core/utils/postprocessing/config.py
@@ -1,14 +1,14 @@
 class Config:
     WORKERS = 16
-    # BASE_DIR = '/home/francesco/Desktop/data/train/'
-    BASE_DIR = '/home/francesco/Desktop/carino/vaevictis/data/test/'
+    BASE_DIR = '/home/francesco/Desktop/data/train/'
+    # BASE_DIR = '/home/francesco/Desktop/carino/vaevictis/data/test/'
     BAG_FOLDER = BASE_DIR + 'bags/'
     CSV_FOLDER = BASE_DIR + 'csvs/'
     MAPS_FOLDER = BASE_DIR + '/maps/'
-    PATCH_SIZE = 80
+    PATCH_SIZE = 100
     ADVANCEMENT_TH = 0.09
     TIME_WINDOW = 100
     DATASET_FOLDER = '/home/francesco/Desktop/data/test/' + 'dataset/{}-{}-{}'.format(100, PATCH_SIZE, ADVANCEMENT_TH) # 100 = n sims
-    HEIGHT_SCALE_FACTOR = 5
+    HEIGHT_SCALE_FACTOR = 1
     SKIP_EVERY = 25
     CENTER_H_PATCH = True
