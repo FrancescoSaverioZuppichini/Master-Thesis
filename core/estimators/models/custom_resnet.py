f98a3434bde1fc777faf3edf4038999470664154
FrancescoSaverioZuppichini <zuppif@usi.ch>
2019-02-04 09:30:21 +0100

    minor changes

--- core/estimators/models/custom_resnet.py
+++ core/estimators/models/custom_resnet.py
@@ -1,12 +1,20 @@
 import torch.nn as nn
 from models.resnet import *
 
+
+class MicroEncoder(ResNetEncoder):
+    @property
+    def blocks_sizes(self):
+        return [(32, 32), (32, 64), (64, 128), (128, 256)]
+
 class MicroResnet(ResNet):
     def __init__(self, in_channel, blocks, block=BasicBlock, conv_layer=nn.Conv2d, n_classes=1000, *args, **kwargs):
         super().__init__(in_channel, blocks, block=block, conv_layer=nn.Conv2d, n_classes=n_classes, *args, **kwargs)
+
+        self.encoder = MicroEncoder(in_channel, blocks, block=block, conv_layer=conv_layer, *args, **kwargs)
         self.encoder.gate = nn.Sequential(
-            conv_layer(in_channel, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(64),
+            conv_layer(in_channel, 32, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(32),
             nn.LeakyReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2)
         )
@@ -14,3 +22,6 @@ class MicroResnet(ResNet):
     @classmethod
     def micro(cls, in_channel, *args, **kwargs):
         return cls(in_channel, blocks=[1, 1, 1, 1], *args, **kwargs)
+
+
+# micro-resnet#1 (32, 32), (32, 64), (64, 128), (128, 256)
