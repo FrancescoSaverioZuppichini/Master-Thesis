7e2a1f510edb72497b47e3f4444d3cd85be04290
Fra <zuppif@usi.ch>
2019-02-07 08:49:27 +0100

    minor changes

--- core/utils/postprocessing/config.py
+++ core/utils/postprocessing/config.py
@@ -10,7 +10,7 @@ class Config:
     TIME_WINDOW = 100
     HEIGHT_SCALE_FACTOR = 1
     SKIP_EVERY = 12
-    DATASET_FOLDER = '/home/francesco/Desktop/data/test/' + 'dataset/{}-{}-{}-{}-no-trick'.format(100,
+    DATASET_FOLDER = '/home/francesco/Desktop/data/train/' + 'dataset/{}-{}-{}-{}-no-trick-06-02-19'.format(100,
                                                                                          PATCH_SIZE,
                                                                                          ADVANCEMENT_TH,
                                                                                          SKIP_EVERY) # 100 = n sims
