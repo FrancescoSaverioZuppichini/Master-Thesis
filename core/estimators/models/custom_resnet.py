ccc1b5a846858f6ff08cfbce9716e72aad3f0668
FrancescoSaverioZuppichini <zuppif@usi.ch>
2019-02-07 22:44:13 +0100

    minor changes

--- core/estimators/models/custom_resnet.py
+++ core/estimators/models/custom_resnet.py
@@ -2,19 +2,19 @@ import torch.nn as nn
 from models.resnet import *
 
 
-class MicroEncoder(ResNetEncoder):
-    @property
-    def blocks_sizes(self):
-        return [(32, 32), (32, 64), (64, 128), (128, 256)]
+# class MicroEncoder(ResNetEncoder):
+#     @property
+#     def blocks_sizes(self):
+#         return [(32, 32), (32, 64), (64, 128), (128, 256)]
 
 class MicroResnet(ResNet):
     def __init__(self, in_channel, blocks, block=BasicBlock, conv_layer=nn.Conv2d, n_classes=1000, *args, **kwargs):
         super().__init__(in_channel, blocks, block=block, conv_layer=nn.Conv2d, n_classes=n_classes, *args, **kwargs)
 
-        self.encoder = MicroEncoder(in_channel, blocks, block=block, conv_layer=conv_layer, *args, **kwargs)
+        # self.encoder = MicroEncoder(in_channel, blocks, block=block, conv_layer=conv_layer, *args, **kwargs)
         self.encoder.gate = nn.Sequential(
-            conv_layer(in_channel, 32, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(32),
+            conv_layer(in_channel, 64, kernel_size=5, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64),
             nn.LeakyReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2)
         )
@@ -25,3 +25,4 @@ class MicroResnet(ResNet):
 
 
 # micro-resnet#1 (32, 32), (32, 64), (64, 128), (128, 256)
+# micro-resnet#2 like resnet but with gate 5x5
