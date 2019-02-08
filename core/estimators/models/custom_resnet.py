bcfacf469775ce1ae2703b76ba1fca4c6315d6ae
FrancescoSaverioZuppichini <zuppif@usi.ch>
2019-02-08 13:24:41 +0100

    minor changes

--- core/estimators/models/custom_resnet.py
+++ core/estimators/models/custom_resnet.py
@@ -2,27 +2,27 @@ import torch.nn as nn
 from models.resnet import *
 
 
-# class MicroEncoder(ResNetEncoder):
-#     @property
-#     def blocks_sizes(self):
-#         return [(32, 32), (32, 64), (64, 128), (128, 256)]
+class MicroEncoder(ResNetEncoder):
+    @property
+    def blocks_sizes(self):
+        return [(16, 32), (32, 64), (64, 128)]
 
 class MicroResnet(ResNet):
     def __init__(self, in_channel, blocks, block=BasicBlock, conv_layer=nn.Conv2d, n_classes=1000, *args, **kwargs):
-        super().__init__(in_channel, blocks, block=block, conv_layer=nn.Conv2d, n_classes=n_classes, *args, **kwargs)
+        super().__init__(in_channel, blocks, block=block, conv_layer=nn.Conv2d, n_classes=n_classes, decoder=MicroEncoder, *args, **kwargs)
 
-        # self.encoder = MicroEncoder(in_channel, blocks, block=block, conv_layer=conv_layer, *args, **kwargs)
         self.encoder.gate = nn.Sequential(
-            conv_layer(in_channel, 64, kernel_size=5, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(64),
+            conv_layer(in_channel, 16, kernel_size=3, stride=1, bias=False),
+            nn.BatchNorm2d(16),
             nn.LeakyReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2)
         )
 
     @classmethod
-    def micro(cls, in_channel, *args, **kwargs):
-        return cls(in_channel, blocks=[1, 1, 1, 1], *args, **kwargs)
+    def micro(cls, in_channel, n=5, *args, **kwargs):
+        return cls(in_channel, blocks=[1 + n, n, n], *args, **kwargs)
 
 
 # micro-resnet#1 (32, 32), (32, 64), (64, 128), (128, 256)
 # micro-resnet#2 like resnet but with gate 5x5
+# micro-resnet#3 [(16, 32), (32, 64), (64, 128)] with [1 + n, n, n]
--- core/estimators/train.py
+++ core/estimators/train.py
@@ -25,32 +25,35 @@ torch.backends.cudnn.deterministic = True
 torch.manual_seed(0)
 
 params = {'epochs': 50,
-          'lr': 0.0001,
+          'lr': 0.001,
           'batch_size': 128,
-          'model': 'microresnet#2',
+          'model': 'microresnet#3-preactivate=True',
           'dataset': '100-100-0.09-12-06-02-19',
+          'test_dataset': '100-100-0.09-12-querry',
           'sampler': None,
           'samper_type': 'sample',
           'callbacks': '[ReduceLROnPlateauCallback]',
           'data-aug': None,
-          'optim': 'sgd',
+          'optim': 'optim',
           'info': 'test as val',
-          'resize': 100 }
+          'resize': 56 }
 
 if torch.cuda.is_available(): torch.cuda.manual_seed_all(0)
 
+
+
 # model = OmarCNN()
-model = MicroResnet.micro(1, n_classes=2, block=[BasicBlock, BasicBlock, BasicBlock, BasicBlockSE], preactivated=True)
-# model = TraversabilityResNet.resnet34(1, n_classes=2)
-# model = resnet34(1, n_classes=2)
-# model.encoder.layers.requires_grad = False
-print(model)
+model = MicroResnet.micro(1,
+                          n_classes=2,
+                          block=[BasicBlock, BasicBlock, BasicBlock, BasicBlock],
+                          preactivated=True)
+# print(model)
 summary(model.cuda(), (1, params['resize'], params['resize']))
 
 criterion = CrossEntropyFlat()
 
 train_dl, val_dl, test_dl = get_dataloaders(train_root='/home/francesco/Desktop/data/train/dataset/{}'.format(params['dataset']),
-                                    test_root='/home/francesco/Desktop/data/test/dataset/{}'.format(params['dataset']),
+                                    test_root='/home/francesco/Desktop/data/test/dataset/{}'.format(params['test_dataset']),
                                     val_size=0,
                                     transform=get_transform(params['resize']),
                                     batch_size=params['batch_size'],
@@ -59,7 +62,6 @@ train_dl, val_dl, test_dl = get_dataloaders(train_root='/home/francesco/Desktop/
                                     pin_memory=True)
 
 data = DataBunch(train_dl=train_dl, valid_dl=test_dl, test_dl=test_dl)
-
 
 print("train size={}, val size={}, test size={}".format(
     len(train_dl) * params['batch_size'],
@@ -75,6 +77,7 @@ experiment.log_parameters(params)
 
 learner = Learner(data=data,
                   model=model,
+                  path='/home/francesco/Desktop/carino/vaevictis/data/',
                   model_dir='/home/francesco/Desktop/carino/vaevictis/data/',
                   loss_func=criterion,
                   opt_func= partial(torch.optim.SGD, momentum=0.95),
@@ -83,17 +86,13 @@ learner = Learner(data=data,
 model_name_acc = '{}-{}-{}-{}-accuracy'.format(params['model'], params['dataset'], params['lr'],  params['resize'])
 model_name_loss = '{}-{}-{}-{}-loss'.format(params['model'], params['dataset'], params['lr'],  params['resize'])
 
-callbacks = [
-            ReduceLROnPlateauCallback(learn=learner, patience=3),
-            EarlyStoppingCallback(learn=learner, patience=10),
+callbacks = [ReduceLROnPlateauCallback(learn=learner, patience=3),
+            EarlyStoppingCallback(learn=learner, patience=5),
             SaveModelCallback(learn=learner, name=model_name_acc, monitor='accuracy'),
             SaveModelCallback(learn=learner, name=model_name_loss)]
 try:
     with experiment.train():
         learner.fit(epochs=params['epochs'], lr=params['lr'], callbacks=callbacks) # SaveModelCallback load the best model after training!
-        # learner.fit(epochs=4, lr=params['lr'], callbacks=callbacks)
-        # model.encoder.layers.requires_grad = True
-        # learner.fit(epochs=10, lr=params['lr']/10, callbacks=callbacks)
 except Exception as e:
     print(e)
     pass
@@ -112,3 +111,6 @@ with experiment.test():
     loss, acc = learner.validate(data.test_dl, metrics=[accuracy])
     print(loss, acc)
     experiment.log_metric("accuracy-from-best-acc", acc.item())
+
+
+torch.save(model, '/home/francesco/Desktop/carino/vaevictis/data/{}.pck'.format(params['model']))
