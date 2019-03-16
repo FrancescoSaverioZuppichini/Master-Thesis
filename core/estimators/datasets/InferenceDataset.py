2a9fe153556e1a229bfda66071d22bb9230ad845
FrancescoSaverioZuppichini <zuppif@usi.ch>
2019-03-16 15:56:51 +0100

    changes in inference and some code refactor
    - now texture in inference is generated using the softmax output

---
+++ core/estimators/datasets/InferenceDataset.py
@@ -0,0 +1,155 @@
+import torch
+import cv2
+import imutils
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import numpy as np
+
+from torch.utils.data import Dataset
+from torchvision.transforms.functional import rotate
+from skimage.util.shape import view_as_windows
+
+class InferenceDataset(Dataset):
+    def __init__(self, hm_path, patch_size=92, step=1, transform=None, rotate=None, debug=False):
+        self.hm = cv2.imread(hm_path)
+        self.hm = cv2.cvtColor(self.hm, cv2.COLOR_BGR2GRAY)
+
+        # rotate the heightmap and them cup the images = to cut the images and then rotate them
+        # if rotate is not None: self.hm = imutils.rotate(self.hm, rotate)
+        self.images = view_as_windows(self.hm, (patch_size, patch_size), step)
+        self.images_shape = self.images.shape
+        self.images = self.images.reshape(-1, patch_size, patch_size)
+        self.transform = transform
+        self.step = step
+        self.patch_size = patch_size
+        self.rotate = rotate
+        self.debug = debug
+        print(self.images_shape)
+
+    def show_patch(self, patch, title):
+        # fig = plt.figure()
+        plt.title(title)
+        sns.heatmap(patch.squeeze())
+        plt.show()
+
+    def __getitem__(self, item):
+        img = self.images[item]
+        if self.debug: self.show_patch(img, 'original')
+
+        if self.rotate is not None: img = imutils.rotate(img, self.rotate)
+        if self.transform is not None: img = self.transform(img)
+
+        if self.debug: self.show_patch(img, 'transform')
+
+        return img, torch.tensor(0)
+
+    def __len__(self):
+        return len(self.images)
+
+    def iter_patches(self, predictions, func):
+        w, h = self.hm.shape
+
+        j = 0
+        for x in range(0, w, self.step):
+            i = 0
+            for y in range(self.step, h, self.step):
+                try:
+                    pred = predictions[i, j]
+                    is_traversable = pred == 0
+
+                    func(x, y, is_traversable)
+                    i += 1
+                except IndexError:
+                    break
+        j += 1
+
+    def add_rects(self, predictions, ax):
+
+        w, h = self.hm.shape
+
+        j = 0
+        for x in range(0, w, self.step):
+            i = 0
+            for y in range(self.step, h, self.step):
+                try:
+                    pred = predictions[i, j]
+                    is_traversable = pred == 0
+
+                    if is_traversable == 0:
+                        rect = mpatches.Rectangle((x, y), self.patch_size,
+                                                  self.patch_size, linewidth=0, edgecolor='none', facecolor='b',
+                                                  alpha=0.1)
+                        ax.add_patch(rect)
+
+                    i += 1
+                except IndexError:
+                    break
+
+            j += 1
+
+    def visualise(self, predictions):
+        fig = plt.figure()
+        plt.title('rotation={}'.format(self.rotate))
+        ax = plt.subplot(1, 1, 1)
+
+        sns.heatmap(self.hm, ax=ax)
+
+        predictions = predictions.reshape(self.images_shape[0], self.images_shape[1])
+
+        self.add_rects(predictions, ax)
+
+        plt.show()
+
+    def make_texture(self, outputs, predictions, name):
+        texture = np.zeros(self.hm.shape)
+        fig = plt.figure()
+        ax = plt.subplot(1, 1, 1)
+
+        predictions = predictions.reshape(self.images_shape[0], self.images_shape[1])
+        outputs = outputs.reshape(self.images_shape[0], self.images_shape[1], 2)
+
+        w, h = self.hm.shape
+        buffer = []
+        j = 0
+        for x in range(0, w, self.step):
+            i = 0
+            for y in range(self.step, h, self.step):
+                try:
+                    pred = predictions[i, j]
+                    out = outputs[i,j]
+                    is_traversable = pred == 1
+                    # TODO understand why they are swapped
+                    if is_traversable: texture[y:y + self.patch_size, x: x + self.patch_size] += out[1]
+
+                    i += 1
+                except IndexError:
+                    break
+            j += 1
+
+
+        # texture = (texture - texture.min()) / (texture.max() - texture.min())
+
+        # texture = cv2.normalize(texture, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+
+        sns.heatmap(texture)
+
+        path = '/home/francesco/Desktop/textures/{}-{}.png'.format(name, self.rotate)
+
+        cv2.imwrite(path, texture)
+
+        return path
+
+
+if __name__ == '__main__':
+    from . import get_transform
+
+    ds = InferenceDataset('/home/francesco/Documents/Master-Thesis/core/maps/test/querry-big-10.png',
+                          patch_size=92,
+                          step=3,
+                          transform=get_transform(None, scale=10), rotate=0, debug=True)
+
+    ds[0]
+    ds[1]
+
--- core/estimators/inference.py
+++ core/estimators/inference.py
@@ -14,152 +14,38 @@ from models import zoo
 from fastai.train import Learner, DataBunch
 from fastai.layers import CrossEntropyFlat
 from datasets.TraversabilityDataset import get_transform
-from models.custom_resnet import *
 from utils import get_learner
-import cv2
-import imutils
 
-class InferenceDataset(Dataset):
-    def __init__(self, hm_path, patch_size=92, step=1, transform=None, rotate=None):
-        self.hm = cv2.imread(hm_path)
-        self.hm = cv2.cvtColor(self.hm, cv2.COLOR_BGR2GRAY)
-        if rotate is not None:
-            self.hm = imutils.rotate(self.hm, rotate)
-        # self.hm = self.hm.astype(np.float32)
-        self.images = view_as_windows(self.hm, (patch_size, patch_size), step)
-        self.images_shape = self.images.shape
-        self.images = self.images.reshape(-1, patch_size, patch_size)
+from datasets.InferenceDataset import InferenceDataset
+
+from torch.nn.functional import softmax
+class Inference():
 
 
-        self.transform = transform
-        self.step = step
-        self.patch_size = patch_size
-        self.rotate = rotate
+    def __call__(self, model_dir, model_name):
+        ds = InferenceDataset('/home/francesco/Documents/Master-Thesis/core/maps/test/querry-big-10.png',
+                              patch_size=92,
+                              step=3,
+                              transform=get_transform(None, scale=10), rotate=270)
 
-        print(self.images_shape)
+        model = zoo[model_name]
+        self.learner = get_learner(model_name, model_dir, callbacks=[], dataset=ds)
 
-    def __getitem__(self, item):
-        img = self.images[item]
-        # img.show()
-        # img.show()
-        if self.transform is not None:
-            img = self.transform(img)
+        outs = self.learner.get_preds(self.learner.data.test_dl)
 
-        # if self.rotate is not None:
-        #     img = imutils.rotate(img, self.rotate)
+        _, preds = torch.max(outs[0], 1)
+        outs = softmax(outs[0], dim=1)
+        path = ds.make_texture(outs.numpy(), preds.numpy(), 'querry')
 
-        return img, torch.tensor(0)
-
-    def __len__(self):
-        return len(self.images)
-
-    def iter_patches(self, predictions, func):
-        w, h = self.hm.shape
-
-        j = 0
-        for x in range(0, w, self.step):
-            i = 0
-            for y in range(self.step, h, self.step):
-                try:
-                    pred = predictions[i, j]
-                    is_traversable = pred == 0
-
-                    func(x, y, is_traversable)
-                    i += 1
-                except IndexError:
-                    break
-        j += 1
-
-    def add_rects(self, predictions, ax):
-
-        w, h = self.hm.shape
-
-        j = 0
-        for x in range(0, w, self.step):
-            i = 0
-            for y in range(self.step, h, self.step):
-                try:
-                    pred = predictions[i, j]
-                    is_traversable = pred == 0
-
-                    if is_traversable == 0:
-                        rect = mpatches.Rectangle((x, y), self.patch_size,
-                                                  self.patch_size, linewidth=0, edgecolor='none', facecolor='b',
-                                                  alpha=0.1)
-                        ax.add_patch(rect)
-
-                    i += 1
-                except IndexError:
-                    break
-
-            j += 1
-
-    def visualise(self, predictions):
-        fig = plt.figure()
-        plt.title('rotation={}'.format(self.rotate))
-        ax = plt.subplot(1, 1, 1)
-
-        sns.heatmap(self.hm, ax=ax)
-
-        predictions = predictions.reshape(self.images_shape[0], self.images_shape[1])
-
-        self.add_rects(predictions, ax)
-
+        tex = cv2.imread(path)
+        plt.imshow(tex)
         plt.show()
-
-    def make_texture(self, predictions, name):
-        texture = np.zeros(self.hm.shape)
-        fig = plt.figure()
-        ax = plt.subplot(1, 1, 1)
-
-        predictions = predictions.reshape(self.images_shape[0], self.images_shape[1])
-
-        w, h = self.hm.shape
-        buffer = []
-        j = 0
-        for x in range(0, w, self.step):
-            i = 0
-            for y in range(self.step, h, self.step):
-                try:
-                    pred = predictions[i, j]
-                    is_traversable = pred == 1
-                    # TODO understand why they are swapped
-                    if is_traversable: texture[y:y + self.patch_size, x: x + self.patch_size] += 1
-
-                    i += 1
-                except IndexError:
-                    break
-            j += 1
-
-        sns.heatmap(texture)
-
-        path = '/home/francesco/Desktop/textures/{}-{}.png'.format(name, self.rotate)
-        if self.rotate is not None:
-            texture= imutils.rotate(texture, -self.rotate)
-        cv2.imwrite(path, texture)
 
         return path
 
-def run_inference(rotate=0):
-    ds = InferenceDataset('/home/francesco/Documents/Master-Thesis/core/maps/test/querry-big-10.png',
-                          patch_size=92,
-                          step=4,
-                          transform=get_transform(None, scale=10), rotate=rotate)
+infer = Inference()
 
-
-    model_dir = 'microresnet#4-gate=3x3-n=2-se=True-750-0.001-None-1552582563.7411294'
-    # model_dir = 'microresnet#4-gate=3x3-n=2-se=True-750-0.001-92-1552226514.811947'
-
-    model_name = 'microresnet#4-gate=3x3-n=2-se=True'
-    model = zoo[model_name]
-
-    learner = get_learner(model_name, model_dir, callbacks=[], dataset=ds)
-
-    outs = learner.get_preds(learner.data.test_dl)
-
-    _, preds = torch.max(outs[0], 1)
-
-    ds.make_texture(preds.numpy(), 'querry-best')
-
-for angle in [0, 90, 180, 270]:
-    run_inference(angle)
+infer('microresnet#4-gate=3x3-n=2-se=True-750-0.001-None-1552582563.7411294',
+                                  'microresnet#4-gate=3x3-n=2-se=True')
+# for angle in [0, 90, 180, 270]:
+#     run_inference(angle)
