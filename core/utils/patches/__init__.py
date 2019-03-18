6a0544f07c8d35842fc429c970fb201e3b034a27
FrancescoSaverioZuppichini <zuppif@usi.ch>
2019-03-18 20:41:11 +0100

    Update __init__.py
    - added bars patch
    - adedd holes patch

--- core/utils/patches/__init__.py
+++ core/utils/patches/__init__.py
@@ -1,11 +1,13 @@
-import numpy as np
+import cv2
 
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 class Patch():
     def __init__(self, size):
         self.hm = np.zeros(size)
+        self.size = size
 
     def __call__(self, *args, **kwargs):
         self.hm = self.make(*args, **kwargs)
@@ -19,10 +21,27 @@ class Patch():
         sns.heatmap(self.hm)
         plt.show()
 
-class BarKrockPatch(Patch):
-    def make(self, *args, **kwargs):
-        pass
+class BarPatch(Patch):
+    def make(self, offset=16, size=4):
+        self.hm[offset: offset + size] = 1
+        self.hm[-offset: -offset + size] = 1
+
+        return self.hm
+
+class HolesPatch(Patch):
+    def make(self):
+        self.hm = cv2.resize(self.hm, (3,3))
+
+        self.hm[self.hm.shape[0] //2,-1] = 1
+
+        self.hm = cv2.resize(self.hm, self.size)
+        return self.hm
 
 
-p = Patch((92,92))
+# p = BarPatch((92,92))
+# p()
+# p.plot2d()
+
+p = HolesPatch((92,92))
+p()
 p.plot2d()
