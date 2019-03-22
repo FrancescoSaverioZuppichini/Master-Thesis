a11fd33b7caba3e9c6d0ea3ae5463659cc2facaa
FrancescoSaverioZuppichini <zuppif@usi.ch>
2019-03-22 19:46:49 +0100

    Update postprocessing.py

--- core/utils/postprocessing/postprocessing.py
+++ core/utils/postprocessing/postprocessing.py
@@ -370,7 +370,7 @@ def make_and_run_chain(config):
 
 def run_train_val_test_chain(base_dir, base_maps_dir, *args, **kwargs):
     for name in ['train', 'val', 'test']:
-        bags_dir = base_dir + '/{}'.format(name) + '/csvs/'
+        base_dir = base_dir + '/{}'.format(name)
         maps_dir = base_maps_dir + '/{}'.format(name)
 
         config = PostProcessingConfig(base_dir=base_dir, maps_folder=maps_dir, name=name, *args, **kwargs)
