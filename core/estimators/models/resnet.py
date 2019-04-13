49dfe7b26bcc6e3330664eb195ad3297633501f5
Fra <zuppif@usi.ch>
2019-04-13 17:44:38 +0200

    introduction
    - minor changes in Resnet

--- core/estimators/models/resnet.py
+++ core/estimators/models/resnet.py
@@ -3,12 +3,12 @@ import torch.nn as nn
 
 from torchvision import models
 
+
 def conv_block3x3(in_planes, out_planes, conv_layer=nn.Conv2d,
                   kernel_size=3, padding=None, preactivate=False, stride=1, activation='relu'):
-
-    activation_funcs= nn.ModuleDict({ 'relu' : nn.ReLU(),
+    activation_funcs = nn.ModuleDict({'relu': nn.ReLU(),
                                       'leaky_relu': nn.LeakyReLU(),
-                                      'selu' : nn.SELU()})
+                                      'selu': nn.SELU()})
 
     padding = kernel_size // 2 if not padding else padding
 
@@ -27,10 +27,12 @@ def conv_block3x3(in_planes, out_planes, conv_layer=nn.Conv2d,
 
     return conv_block
 
+
 class SEModule(nn.Module):
     """
     Squeeze and Excitation module https://arxiv.org/abs/1709.01507
     """
+
     def __init__(self, n_features, ratio=16, *args, **kwargs):
         super().__init__()
 
@@ -50,10 +52,11 @@ class SEModule(nn.Module):
 
         return x * out
 
+
 class BasicBlock(nn.Module):
     """
-    Basic block of ResNet, is is composed by two 3x3 conv - relu and batchnorm.
-    It automatically perform residual addition
+    Basic block of ResNet, it is composed by two 3x3 conv - relu and batchnorm.
+    It automatically performs residual addition
     """
     expansion = 1
 
@@ -73,7 +76,7 @@ class BasicBlock(nn.Module):
     def get_shortcut(self):
         return nn.Sequential(
             nn.Conv2d(self.in_planes, self.out_planes, kernel_size=1,
-                       stride=self.stride, bias=False),
+                      stride=self.stride, bias=False),
             nn.BatchNorm2d(self.out_planes),
         )
 
@@ -82,7 +85,6 @@ class BasicBlock(nn.Module):
             conv_block(self.in_planes, out_planes, stride=stride, *args, **kwargs),
             conv_block(out_planes, out_planes, *args, **kwargs),
         )
-
 
     def forward(self, x):
         residual = x
@@ -94,6 +96,7 @@ class BasicBlock(nn.Module):
         out = out + residual
 
         return out
+
 
 class Bottleneck(BasicBlock):
     expansion = 4
@@ -107,8 +110,8 @@ class Bottleneck(BasicBlock):
 
 
 class BasicBlockSE(BasicBlock):
-    def __init__(self, in_planes, out_planes,  *args, **kwargs):
-        super().__init__(in_planes, out_planes,  *args, **kwargs)
+    def __init__(self, in_planes, out_planes, *args, **kwargs):
+        super().__init__(in_planes, out_planes, *args, **kwargs)
         self.se = SEModule(out_planes)
 
     def forward(self, x):
@@ -145,11 +148,12 @@ class BottleneckSE(Bottleneck):
 
 class ResNetLayer(nn.Module):
     """
-    This class represent a layer of ResNet, it stacks a number of block
+    This class represent a layer of ResNet, it stacks a number of blocks
     equal to the depth parameter. If the `in_planes` and `out_planes` are different,
     for example 64 features in and 128 features out, the first layer will use a stride of two,
     quoting from the paper: 'We perform downsampling directly by convolutional layers that have a stride of 2.'
     """
+
     def __init__(self, in_planes, out_planes, depth, block=BasicBlock, *args, **kwargs):
         super().__init__()
         # if inputs == outputs then stride = 1, e.g 64==64 (first block)
@@ -174,7 +178,9 @@ class ResNetEncoder(nn.Module):
     This class represents the head of ResNet. It reduce the dimension of the input image by apply the
     .gate layer first and feed the output to one layer after the other.
     """
-    def __init__(self, in_channel, depths, blocks=BasicBlock, blocks_sizes=None, conv_block=conv_block3x3, *args, **kwargs):
+
+    def __init__(self, in_channel, depths, blocks=BasicBlock, blocks_sizes=None, conv_block=conv_block3x3, *args,
+                 **kwargs):
         super().__init__()
         self.in_channel, self.depths, self.blocks = in_channel, depths, blocks
 
@@ -191,7 +197,7 @@ class ResNetEncoder(nn.Module):
             self.blocks = [blocks] * len(self.blocks_sizes)
         # stack a number of layers together equal to the len of depth
         self.layers = nn.ModuleList([
-            ResNetLayer(in_c, out_c, depth=depths[i], block=self.blocks[i],  *args, **kwargs)
+            ResNetLayer(in_c, out_c, depth=depths[i], block=self.blocks[i], *args, **kwargs)
             for i, (in_c, out_c) in enumerate(self.blocks_sizes)
         ])
 
@@ -215,11 +221,13 @@ class ResNetEncoder(nn.Module):
 
         return x
 
+
 class ResnetDecoder(nn.Module):
     """
     This class represents the tail of ResNet. It performs a global pooling and maps the output to the
     correct class by using a fully connected layer.
     """
+
     def __init__(self, in_features, n_classes):
         super().__init__()
         self.avg = nn.AdaptiveAvgPool2d((1, 1))
@@ -231,21 +239,22 @@ class ResnetDecoder(nn.Module):
         x = self.decoder(x)
         return x
 
+
 class ResNet(nn.Module):
     """
     ResNet https://arxiv.org/pdf/1512.03385.pdf
     """
-    def __init__(self, depths, in_channel=3, blocks=BasicBlock, n_classes=1000, encoder=ResNetEncoder, decoder=ResnetDecoder, *args, **kwargs):
+
+    def __init__(self, depths, in_channel=3, blocks=BasicBlock, n_classes=1000, encoder=ResNetEncoder,
+                 decoder=ResnetDecoder, *args, **kwargs):
         super().__init__()
         self.depths, self.in_channel, self.n_classes = depths, in_channel, n_classes
-        self.encoder = encoder(in_channel,depths, blocks, *args, **kwargs)
+        self.encoder = encoder(in_channel, depths, blocks, *args, **kwargs)
         self.decoder = decoder(self.encoder.blocks_sizes[-1][1] * self.encoder.blocks[-1].expansion, n_classes)
 
     def forward(self, x):
         return self.decoder(self.encoder(x))
 
-
-# resnet-tiny = [1, 2, 3, 2]
 
 def resnet18(in_channel=3, blocks=BasicBlock, resnet=ResNet, pretrained=False, *args, **kwargs):
     model = resnet([2, 2, 2, 2], in_channel, blocks, *args, **kwargs)
@@ -256,6 +265,7 @@ def resnet18(in_channel=3, blocks=BasicBlock, resnet=ResNet, pretrained=False, *
 
     return model
 
+
 def resnet34(in_channel=3, blocks=BasicBlock, pretrained=False, resnet=ResNet, **kwargs):
     model = resnet([3, 4, 6, 3], in_channel, blocks, **kwargs)
 
@@ -265,13 +275,15 @@ def resnet34(in_channel=3, blocks=BasicBlock, pretrained=False, resnet=ResNet, *
 
     return model
 
-def resnet50(in_channel=3, blocks=Bottleneck,  pretrained=False, resnet=ResNet, **kwargs):
+
+def resnet50(in_channel=3, blocks=Bottleneck, pretrained=False, resnet=ResNet, **kwargs):
     model = resnet([3, 4, 6, 3], in_channel, blocks, **kwargs)
     if pretrained:
         print('loading trained weights...')
         restore(models.resnet50(True), model)
 
     return model
+
 
 def resnet101(in_channel=3, blocks=Bottleneck, pretrained=False, resnet=ResNet, **kwargs):
     model = resnet([3, 4, 23, 3], in_channel, blocks, **kwargs)
@@ -280,6 +292,7 @@ def resnet101(in_channel=3, blocks=Bottleneck, pretrained=False, resnet=ResNet, 
         restore(models.resnet101(True), model)
     return model
 
+
 def resnet152(in_channel=3, blocks=Bottleneck, pretrained=False, resnet=ResNet, **kwargs):
     model = resnet([3, 8, 36, 3], in_channel, blocks, **kwargs)
     if pretrained:
@@ -287,13 +300,14 @@ def resnet152(in_channel=3, blocks=Bottleneck, pretrained=False, resnet=ResNet, 
         restore(models.resnet152(True), model)
     return model
 
+
 def restore(source, target):
     pre_trained_layers = [source.layer1, source.layer2, source.layer3, source.layer4]
 
-    for i,pre_trained_layer in enumerate(pre_trained_layers):
+    for i, pre_trained_layer in enumerate(pre_trained_layers):
         layer = target.encoder.layers[i]
-        p_t_convs = [m for m in  pre_trained_layer.modules() if isinstance(m, nn.Conv2d)]
-        p_t_bns = [m for m in  pre_trained_layer.modules() if isinstance(m, nn.BatchNorm2d)]
+        p_t_convs = [m for m in pre_trained_layer.modules() if isinstance(m, nn.Conv2d)]
+        p_t_bns = [m for m in pre_trained_layer.modules() if isinstance(m, nn.BatchNorm2d)]
 
         convs = [m for m in layer.modules() if isinstance(m, nn.Conv2d)]
         bns = [m for m in layer.modules() if isinstance(m, nn.BatchNorm2d)]
