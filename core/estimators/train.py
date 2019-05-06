4f64536b2cd64fed66cf9cec564fe04bbe93001c
Fra <zuppif@usi.ch>
2019-05-06 14:30:59 +0200

    minor changes

--- core/estimators/train.py
+++ core/estimators/train.py
@@ -176,9 +176,9 @@ if __name__ == '__main__':
               'problem' : 'classification',
               'more_than': None,
               'down_sampling': 2,
-              'time_window': 50 * 2,
+              'time_window': 50 * 3,
               'only_forward': False,
-              'patch_size': 0.66  }
+              'patch_size': 1  }
 
     # params['data-aug'] = False
     #
@@ -191,7 +191,8 @@ if __name__ == '__main__':
     #     train_and_evaluate(params)
 
     params['data-aug'] = True
-    params['data-aug-type'] = 'Dropout(p=(0.05, 0.1))-CoarseDropout((0.02, 0.1),(0.6, 0.8))-RandomSimplexNoise(1, 50)(4,8)',
+    params['data-aug-type'] = 'Dropout(p=(0.05, 0.1))-CoarseDropout((0.02, 0.1),(0.6, 0.8))',
+    # -RandomSimplexNoise(1, 50)(4, 8)
 
     for _ in range(5):
         train_and_evaluate(params)
